0ef0a9d11bce9b42b56c8ee3b4ac18a7d19735b7
jaimalharsk <satishmailid@gmail.com>
2025-05-10 18:48:17 +0530

    budge optim

--- cloud_mgmt_tool/subscription_manager/admin.py
+++ cloud_mgmt_tool/subscription_manager/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser, CloudAccount, BudgetSetting, Subscription
+from .models import CustomUser, CloudAccount,Subscription,Budget
 
 # Custom User Admin
 class CustomUserAdmin(UserAdmin):
@@ -20,19 +20,25 @@ class CloudAccountAdmin(admin.ModelAdmin):
     list_filter = ('provider',)
 
 # Budget Setting Admin
-class BudgetSettingAdmin(admin.ModelAdmin):
-    list_display = ('user', 'provider', 'monthly_budget', 'alert_threshold', 'created_on')
-    list_filter = ('provider', 'created_on')
-    search_fields = ('user__username',)
+# class BudgetSettingAdmin(admin.ModelAdmin):
+#     list_display = ('user', 'provider', 'monthly_budget', 'alert_threshold', 'created_on')
+#     list_filter = ('provider', 'created_on')
+#     search_fields = ('user__username',)
 
 # Subscription Admin
 class SubscriptionAdmin(admin.ModelAdmin):
-    list_display = ('user', 'name', 'cost', 'renewal_date', 'status', 'frequency')
+    list_display = ('user', 'service_name', 'price', 'renewal_date', 'status', 'frequency')  # changed 'cost' → 'price'
     list_filter = ('status', 'frequency', 'renewal_date')
     search_fields = ('user__username', 'name')
+
+class BudgetAdmin(admin.ModelAdmin):
+    list_display = ('user', 'cloud_provider', 'monthly_budget', 'alert_threshold', 'created_at')
+    list_filter = ('cloud_provider',)
+    search_fields = ('user__username',)
 
 # Register models with admin site
 admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(CloudAccount, CloudAccountAdmin)
-admin.site.register(BudgetSetting, BudgetSettingAdmin)
+#admin.site.register(BudgetSetting, BudgetSettingAdmin)
 admin.site.register(Subscription, SubscriptionAdmin)
+admin.site.register(Budget, BudgetAdmin)
